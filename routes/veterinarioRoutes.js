b4cc53ace9bc9cbdc6e4429d46325f1d2ebc9f67
Oviedo84 <oviedoinfinite@hotmail.com>
2022-06-07 14:24:37 -0500

    Levantando el servidor

---
+++ routes/veterinarioRoutes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+const router = express.Router();
+import { registrar, perfil, confirmar, autenticar, olvidePassword, comprobarToken, nuevoPassword } from  '../controllers/veterinarioController.js';
+import checkAuth from '../middleware/authMiddleware.js';
+
+// Área Pública
+router.post('/', registrar);
+router.get('/confirmar/:token', confirmar);
+router.post('/login', autenticar);
+router.post('/olvide-password', olvidePassword);
+router.get('/olvide-password/:token', comprobarToken);
+router.post('/olvide-password/:token', nuevoPassword);
+
+// Área Privada
+router.get('/perfil', checkAuth, perfil);
+
+export default router;
